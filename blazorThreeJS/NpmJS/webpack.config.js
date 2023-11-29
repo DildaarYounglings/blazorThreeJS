897f5b97061642463adea76be3f9a42118243292
primedildaar2 <primedildaar2@gmail.com>
2023-11-29 15:29:43 +0200

    fixed the webpack.config.js file

---
+++ blazorThreeJS/NpmJS/webpack.config.js
@@ -0,0 +1,8 @@
+﻿let path = require("path");
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main_three.js",
+        path: path.resolve(__dirname, "JS"),
+    }
+}
